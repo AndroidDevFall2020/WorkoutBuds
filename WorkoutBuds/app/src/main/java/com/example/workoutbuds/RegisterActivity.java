e5aca9d23842936c5dc04c2e6ca55ea83616a5c3
Sakinah Cottman <sakinah.cottman@yahoo.com>
2020-11-21 20:49:08 -0500

    added sign up page and fixed font

--- WorkoutBuds/app/src/main/java/com/example/workoutbuds/RegisterActivity.java
+++ WorkoutBuds/app/src/main/java/com/example/workoutbuds/RegisterActivity.java
@@ -2,13 +2,65 @@ package com.example.workoutbuds;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    public static final String TAG = "RegisterActivity";
+    private EditText etUsername;
+    private EditText etPassword;
+    private EditText etEmail;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etPassword = findViewById(R.id.etPassword);
+        etUsername = findViewById(R.id.etUsername);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        etEmail = findViewById(R.id.etEmail);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "onClick Submit Button");
+                ParseUser user = new ParseUser();
+                user.setUsername(etUsername.getText().toString());
+                user.setPassword(etPassword.getText().toString());
+                user.setEmail(etEmail.getText().toString());
+                user.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e != null){
+                            Log.e(TAG, "issue with signing up", e);
+                            Toast.makeText(RegisterActivity.this, "Issue with sign up", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        else{
+                            goMainActivity();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+
+    private void goMainActivity(){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
