1ebbf5713fddf064540e397bb0942cfc8f01788b
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-11-29 05:10:45 -0500

    Created UIfor gc Screen

--- WorkoutBuds/app/src/main/java/com/example/workoutbuds/fragments/GroupChatsFragment.java
+++ WorkoutBuds/app/src/main/java/com/example/workoutbuds/fragments/GroupChatsFragment.java
@@ -2,15 +2,33 @@ package com.example.workoutbuds.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.workoutbuds.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class GroupChatsFragment extends Fragment {
+
+    public static final String TAG = "GroupChatsFragment";
+
+    private RecyclerView rvGroupChats;
+
+    public GroupChatsFragment() {
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,4 +36,25 @@ public class GroupChatsFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_group_chats, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        rvGroupChats = view.findViewById(R.id.rvGroupChats);
+    }
+
+    private void queryGroups() {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("GroupChat");
+        query.include("author");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG, "Issue with retrieving groups", e);
+                }
+            }
+        });
+    }
+
 }
