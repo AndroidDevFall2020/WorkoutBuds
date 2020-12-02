375780381d14425a186b8d6c4c7511168a351422
popoolasubomi <popoolaogooluwasubomi@gmail.com>
2020-12-02 11:46:48 -0500

    Created ComposePostActivity

---
+++ WorkoutBuds/app/src/main/java/com/example/workoutbuds/ComposePostActivity.java
@@ -0,0 +1,14 @@
+package com.example.workoutbuds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComposePostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_post);
+    }
+}
